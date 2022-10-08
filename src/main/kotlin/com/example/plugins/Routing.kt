15fea807c7c1a1452d700cbf2d5077cef399e813
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2022-10-08 13:10:39 +0900

    Order endpoints

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -8,5 +8,8 @@ import com.example.routes.*
 fun Application.configureRouting() {
     routing {
         customerRouting()
+        listOrdersRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
     }
 }
